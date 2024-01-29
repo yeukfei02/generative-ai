004b7c2036e580bd6cf9b5afb89680f6c61f685f
Donald Wu <yeukfei02@gmail.com>
2024-01-29 17:41:57 +0800

    fix: update env

--- helper/helper.py
+++ helper/helper.py
@@ -2,7 +2,7 @@ import aws_cdk as cdk
 import os
 
 def get_env():
-    account_id = os.getenv("account_id")
+    account_id = os.getenv("ACCOUNT_ID")
     env = cdk.Environment(account=account_id, region="us-east-1")
 
     return env
